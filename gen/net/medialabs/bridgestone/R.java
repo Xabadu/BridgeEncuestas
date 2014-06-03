911560c4b1cc13df4ded5eb8f28dad3249cb496d
Fernando Larrañaga <xabadu@gmail.com>
2014-06-03 16:27:21 -0400

    Add: - Text questions to the view programmatically - Next and back buttons

--- gen/net/medialabs/bridgestone/R.java
+++ gen/net/medialabs/bridgestone/R.java
@@ -21,13 +21,25 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bg_encuestas=0x7f020000;
+        public static final int btn_siguiente=0x7f020001;
+        public static final int btn_volver=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int img_bg_topbar=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f08000b;
+        public static final int btnSiguiente=0x7f080009;
+        public static final int btnVolver=0x7f08000a;
+        public static final int contenedorPreguntas=0x7f080007;
         public static final int guardarDatosBtn=0x7f080002;
+        public static final int imageView1=0x7f080003;
         public static final int listadoTiendas=0x7f080001;
         public static final int listadoTiendasLabel=0x7f080000;
+        public static final int textView1=0x7f080004;
+        public static final int textView2=0x7f080005;
+        public static final int textView3=0x7f080006;
+        public static final int tituloPregunta=0x7f080008;
     }
     public static final class layout {
         public static final int activity_configuration=0x7f030000;
@@ -42,8 +54,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int breadcrumb_encuestas=0x7f050008;
         public static final int configuration_label=0x7f050007;
         public static final int hello_world=0x7f050004;
+        public static final int label_encuesta_standard=0x7f050009;
         public static final int save_data_btn=0x7f050006;
         public static final int title_activity_configuration=0x7f050003;
         public static final int title_activity_encuesta=0x7f050005;
--- src/net/medialabs/bridgestone/EncuestaActivity.java
+++ src/net/medialabs/bridgestone/EncuestaActivity.java
@@ -1,15 +1,113 @@
 package net.medialabs.bridgestone;
 
-import android.os.Bundle;
+import java.io.IOException;
+
+import net.medialabs.utilities.Alertas;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class EncuestaActivity extends Activity {
+	
+	private static final String SERVICE_BASE_URL = "http://www.solucionesparche.com/labs/bridgestone/index.php/servicios/";
+	private static final String SERVICE_FORMAT = "format/json/";
+	private static final String PREFERENCES_FILE = "net.medialabs.bridgestone.PREFERENCE_FILE_KEY";
+	private JSONArray preguntasArray;
+	private JSONArray opcionesArray;
+	private JSONArray opcionesPreguntaArray;
+	private JSONObject resultObject;
+	private JSONObject preguntaSimple;
+	private int numeroPregunta;
+	EditText campoRespuesta;
+	ImageButton btnSiguiente;
+	ImageButton btnVolver;
+	LinearLayout contenedorPreguntas;
+	TextView enunciadoPregunta;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		EncuestaActiva encuesta = new EncuestaActiva(this);
+		encuesta.execute();
+	}
+	
+	private void mostrarEncuesta(final JSONObject encuesta, int numero) {
 		setContentView(R.layout.activity_encuesta);
+		numeroPregunta = numero;
+		btnSiguiente = (ImageButton) findViewById(R.id.btnSiguiente);
+		btnVolver = (ImageButton) findViewById(R.id.btnVolver);
+		try {
+			preguntasArray = encuesta.getJSONArray("preguntas");
+			preguntaSimple = preguntasArray.getJSONObject(numeroPregunta);
+			enunciadoPregunta = (TextView) findViewById(R.id.tituloPregunta);
+			enunciadoPregunta.setText(preguntaSimple.getString("enunciado"));
+			contenedorPreguntas = (LinearLayout) findViewById(R.id.contenedorPreguntas);
+			if(preguntaSimple.getString("tipo").equalsIgnoreCase("TEXT")) {
+				campoRespuesta = new EditText(this);
+				campoRespuesta.setWidth(LayoutParams.MATCH_PARENT);
+				campoRespuesta.setHeight(LayoutParams.WRAP_CONTENT);
+				contenedorPreguntas.addView(campoRespuesta);
+			} else {
+				opcionesArray = encuesta.getJSONArray("opciones");
+				opcionesPreguntaArray = opcionesArray.getJSONArray(numeroPregunta);
+				
+				if(preguntaSimple.getString("tipo").equalsIgnoreCase("SELECT")) {
+					
+				} else if(preguntaSimple.getString("tipo").equalsIgnoreCase("RADIO")) {
+					
+				} else if(preguntaSimple.getString("tipo").equalsIgnoreCase("CHECKBOX")) {
+					
+				} else if(preguntaSimple.getString("tipo").equalsIgnoreCase("ICONS")) {
+					
+				}
+				
+			} 
+			
+				
+			
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		
+		btnSiguiente.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				numeroPregunta++;
+				if(numeroPregunta <= preguntasArray.length()) {
+					mostrarEncuesta(encuesta, numeroPregunta);
+				} else {
+					// procesar respuestas
+				}
+			}
+		});
+		
+		btnVolver.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				EncuestaActiva encuesta = new EncuestaActiva(EncuestaActivity.this);
+				encuesta.execute();
+			}
+		});
+		
 	}
 
 	@Override
@@ -17,6 +115,63 @@ public class EncuestaActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.encuesta, menu);
 		return true;
+	}
+	
+	private class EncuestaActiva extends AsyncTask<Void, Void, String> {
+		
+		private ProgressDialog dialog;
+		private Alertas alerta = new Alertas();
+		private EncuestaActivity activityRef;
+		private JSONObject encuestaObject;
+		
+		public EncuestaActiva(EncuestaActivity activityRef) {
+			this.activityRef = activityRef;
+		}
+		
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			dialog = ProgressDialog.show(activityRef, "", "Cargando encuesta...", true);	
+		}
+		
+		@Override
+		protected String doInBackground(Void... params) {
+			HttpClient client = new DefaultHttpClient();
+			HttpGet get = new HttpGet(SERVICE_BASE_URL + "encuestas/activa/" + SERVICE_FORMAT);
+			get.setHeader("content-type", "application/json");
+			
+			try {
+				HttpResponse response = client.execute(get);
+				return EntityUtils.toString(response.getEntity());
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			if(result == null) {
+				alerta.showAlertDialog(activityRef, "Error", "No hay servicios disponibles", false);
+				dialog.dismiss();
+			} else {
+				try {
+					resultObject = new JSONObject(result);
+					if(resultObject.getString("status").equalsIgnoreCase("OK")) {
+						encuestaObject = resultObject.getJSONObject("response");
+						mostrarEncuesta(encuestaObject, 0);
+					}
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				dialog.dismiss();
+			}
+			
+		}
+		
 	}
 
 }
